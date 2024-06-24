6811397a3eefd08bb29ea7cf05941d968a3a06cb
AzlanAmjad <Azlanamjad_123@hotmail.com>
2024-06-23 22:27:07 -0600

    portfolio project setup

---
+++ portfolio/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <p>Azlan Amjad</p>
+        </>
+    );
+}
+
+export default App;
---
+++ portfolio/frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <nav>
+            <h1>Navbar</h1>
+        </nav>
+    );
+}
+
+export default NavBar;
