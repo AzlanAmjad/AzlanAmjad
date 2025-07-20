0b8527a77c3a8d8a2ff4e1771433317280b2a5fc
AzlanAmjad <Azlanamjad_123@hotmail.com>
2025-07-20 12:37:09 -0600

    remove vercel analytics

--- portfolio/frontend/src/components/Layout.jsx
+++ portfolio/frontend/src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
 
 function Layout() {
     return (
         <div className="relative flex flex-col justify-between min-h-screen font-cormorantGaramond bg-night text-white overflow-hidden">
-            <Analytics />
             <NavBar />
             <main className="p-14 mt-16 sm:mt-28 relative z-10">
                 <Outlet />
