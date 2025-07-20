eca696f4adb5b6669b9882a51a521f1f18defbbf
AzlanAmjad <Azlanamjad_123@hotmail.com>
2025-07-20 12:29:23 -0600

    remove flare and add vercel analytics

--- portfolio/frontend/src/components/Layout.jsx
+++ portfolio/frontend/src/components/Layout.jsx
@@ -1,13 +1,12 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import Flare from "./Flare";
+import { Analytics } from "@vercel/analytics/next"
 
 function Layout() {
     return (
         <div className="relative flex flex-col justify-between min-h-screen font-cormorantGaramond bg-night text-white overflow-hidden">
-            <Flare className="w-56 h-40 left-[20%] top-[28%] bg-lightBlue blur-3xl z-0" />
-            <Flare className="w-44 h-32 left-[60%] top-[60%] bg-lightBlue blur-3xl z-0" />
+            <Analytics />
             <NavBar />
             <main className="p-14 mt-16 sm:mt-28 relative z-10">
                 <Outlet />
